7eb01b473697df9683df053056433b5e62c5fe26
KaiXuan <2327649721@qq.com>
2026-06-29 13:05:51 +0800

    init: 项目初始化

---
+++ src/main/java/com/kaixuan/agentreproxy/AgentreproxyApplication.java
@@ -0,0 +1,13 @@
+package com.kaixuan.agentreproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentreproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentreproxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaixuan/agentreproxy/AgentreproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaixuan.agentreproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentreproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
